738c2ff8124caf22d35e5978e071a5ac3d78cf3c
Alberto Cole <acpii2005@gmail.com>
2013-09-02 16:41:46 -0600

    Servicio de Productos con Lawnchair

--- app/scripts/controllers/productos.js
+++ app/scripts/controllers/productos.js
@@ -3,7 +3,7 @@
 angular.module('superZapatoGapApp')
   .controller('ProductosCtrl', ['$scope','productoService',function ($scope,productoService) {
     $scope.guardarProducto = function(){
-      productoService.guardar($scope.producto).then(function(producto){
+      productoService.guardar($scope.producto).then(function(){
         $scope.producto = {};
         $scope.productoGuardado = true;
       });

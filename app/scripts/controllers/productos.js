47513b0bf4221e196192fe8f452e268d42be09d6
Alberto Cole <acpii2005@gmail.com>
2013-09-02 15:02:33 -0600

    Tirando Vista de Usuarios

---
+++ app/scripts/controllers/productos.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('superZapatoGapApp')
+  .controller('ProductosCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });

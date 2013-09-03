e27d54e8e5193ec66f1c8e0376e2005255f23799
Alberto Cole <acpii2005@gmail.com>
2013-09-03 08:36:15 -0600

    Agregando Info de Sliders e Imagen Principal al producto

--- app/scripts/controllers/productos.js
+++ app/scripts/controllers/productos.js
@@ -2,10 +2,17 @@
 
 angular.module('superZapatoGapApp')
   .controller('ProductosCtrl', ['$scope','productoService',function ($scope,productoService) {
+    $scope.producto = {imagenesSlide:[{path:'',ultimo:true}]};
     $scope.guardarProducto = function(){
       productoService.guardar($scope.producto).then(function(){
-        $scope.producto = {};
+        $scope.producto = {imagenesSlide:[{path:'',ultimo:true}]};
         $scope.productoGuardado = true;
       });
     };
+    $scope.agregarSlide = function(){
+      _.each($scope.producto.imagenesSlide,function(slide){
+        slide.ultimo = false;
+      });
+      $scope.producto.imagenesSlide.push({path:'',ultimo:true});
+    }
   }]);

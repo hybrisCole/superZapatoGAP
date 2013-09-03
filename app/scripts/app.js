1e3ceaf659c4cb509f6a417256cdce8def72e30c
rsobrado <rsobrado@pzfullparty.com>
2013-09-03 16:56:46 -0600

    display de productos y un par de cosas mas

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -19,3 +19,22 @@ angular.module('superZapatoGapApp', [])
         redirectTo: '/'
       });
   });
+
+$.each(data.photoset.photo, function(i,photo){ 
+
+          var images ='<img src="http://farm5.static.flickr.com/'+photo.server+'/'+photo.id+'_'+photo.secret+'_m.jpg" class="box myphotos" rel="'+PS_ID+'" data-glisse-big="http://farm5.static.flickr.com/'+photo.server+'/'+photo.id+'_'+photo.secret+'_b.jpg" title="'+photo.title+'" />';
+
+          $('#sets').append(images);  
+
+          $('#sets').masonry({
+            columnWidth: 300,
+            itemSelector: '.box', 
+            isAnimated: true,
+            animationOptions: {
+              duration: 1000,
+              queue: false  
+            }  
+          }).imagesLoaded(function() {
+            $('#sets').masonry('reload');
+            });
+      });

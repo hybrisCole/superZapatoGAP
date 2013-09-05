15df9e1fab6fda81f72d023c0ef942663e274e2e
Alberto Cole <acpii2005@gmail.com>
2013-09-05 02:14:18 -0600

    Agregando el json de productos base

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,10 +1,11 @@
 'use strict';
 
 angular.module('superZapatoGapApp')
-  .controller('MainCtrl', function ($scope) {
+  .controller('MainCtrl', function ($scope,productoService) {
     $scope.urls = [
       '/shoppingCart',
       '/productos',
     ];
+    productoService.cargar();
   });
 
